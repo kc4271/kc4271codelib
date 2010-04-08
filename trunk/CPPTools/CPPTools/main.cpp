fbfad169d7d9639b394e47a4896857116305e580
kc4271 <kc4271@47a20231-dca5-1af3-f555-f7f3b0b382d2>
2010-04-08 11:24:35 +0000

    更改了函数名MemReverse->memrevrs

--- trunk/CPPTools/CPPTools/main.cpp
+++ trunk/CPPTools/CPPTools/main.cpp
@@ -6,17 +6,17 @@ using namespace std;
 int main()
 {
 	char A[] = "1234";
-	char B[] = "12345";
+	char B[] = "12345\0";
 	unsigned char *e = NULL;
 	int c = -10;
-	MemReverse((unsigned char *)&c,4);
-	MemReverse((unsigned char *)&c,4);
-	MemReverse((unsigned char *)A,4);
-	MemReverse((unsigned char *)A,(unsigned char *)A+3);
-	MemReverse((unsigned char *)B,(unsigned char *)B+4);
-	MemReverse((unsigned char *)B,5);
-	MemReverse(e,e+3);
-	MemReverse(e,99999);
+	memrevrs((unsigned char *)&c,4);
+	memrevrs((unsigned char *)&c,4);
+	memrevrs((unsigned char *)A,4);
+	memrevrs((unsigned char *)A,(unsigned char *)A+3);
+	memrevrs((unsigned char *)B,(unsigned char *)B+4);
+	memrevrs((unsigned char *)B,strlen(B));
+	memrevrs(e,e+3);
+	memrevrs(e,99999);
 	cout<<A<<endl<<B<<endl<<c<<endl;
 	return 0;
 }
