402d2cc4a896f5f3105f565474a8a8d95dad6e95
kc4271 <kc4271@47a20231-dca5-1af3-f555-f7f3b0b382d2>
2010-04-15 13:21:51 +0000

    1.增加了模板函数revrs,用来进行如int、float、double等的大小端转换。 2.增加了模板函数read_big_endien_value,可以从标准流读入大端形式存的数据并存放在int、float等类型中。

--- CPPTools/CPPTools/CPPTools.h
+++ CPPTools/CPPTools/CPPTools.h
@@ -27,3 +27,19 @@ bool memrevrs(unsigned char *pBegin,unsigned int size)
 	}
 	return  true;
 }
+
+template <class Type>
+void revrs(Type &inValue)
+{
+	memrevrs((unsigned char*)&inValue,sizeof(inValue));
+}
+
+template <class Type>
+void read_big_endien_value(std::istream &in,Type &v)
+{
+	unsigned int len = sizeof(v);
+	for(int i = len - 1;i >= 0;i--)
+	{
+		in.read(((char *)&v)+i,sizeof(char));
+	}
+}
