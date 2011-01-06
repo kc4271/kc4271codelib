0758455407b98803400e24b7fc02265f1d7098bf
kc4271 <kc4271@47a20231-dca5-1af3-f555-f7f3b0b382d2>
2011-01-06 08:21:03 +0000

    对Ktrace进行了微小的修改，析构时使KTrace.exe自动退出，大部分时候可以使用，尚未提供C接口。

--- KTrace/KTrace.cpp
+++ KTrace/KTrace.cpp
@@ -5,9 +5,9 @@ int main( int argc, char *argv[] )
 	int pread;
 	int bufsize;
 	char *buf;
-	printf("KTRACE BEGIN!\n");
 	if(argc == 3)
 	{
+		printf("KTRACE BEGIN!\n");
 		pread = atoi(argv[1]);
 		bufsize = atoi(argv[2]);
 		buf = (char *)malloc(sizeof(char) * (bufsize + 1));
@@ -15,9 +15,9 @@ int main( int argc, char *argv[] )
 		{
 			_read(pread,buf,bufsize);
 			buf[bufsize] = 0;
-			printf("%s",buf);
 			if(strcmp(buf,"__EOF__") == 0)
 				break;
+			printf("%s",buf);
 		}
 		free(buf);
 	}
